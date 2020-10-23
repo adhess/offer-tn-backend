4b63d2f4c207950bc12c76d51f5d5d0701141364
adhess <nicorobine@outlook.fr>
2020-10-23 23:21:58 +0100

    populate db

---
+++ app/management/commands/mock_data.py
@@ -0,0 +1,175 @@
+
+mock_data = [
+    {
+        "name": "Telephonie & Tablette", "children": [
+        {
+            "name": "Smartphone & Mobile", "children": [
+            {"name": "Telephone Portable", "path": ""},
+            {"name": "Smartphones Android", "path": ""},
+            {"name": "Iphone", "path": ""}
+        ]
+        }, {
+            "name": "Tablette & phablette", "children": [
+                {"name": "Tablette", "path": ""},
+                {"name": "Phablette", "path": ""},
+                {"name": "Ipad", "path": ""}
+            ]
+        }, {
+            "name": "Telephone fixe & fax", "children": [
+                {"name": "Telephone filaire", "path": ""},
+                {"name": "Telephone sans fil", "path": ""},
+                {"name": "Fax Lser", "path": ""},
+                {"name": "Fax Thermique", "path": ""}
+            ]
+        }, {
+            "name": "Accessoires telephonie", "children": [
+                {"name": "Etui & coque", "path": ""},
+                {"name": "Protection ecran", "path": ""},
+                {"name": "Ecouteurs et kit bluetooth", "path": ""},
+                {"name": "Power Bank", "path": ""},
+                {"name": "Chargeur & Batterie", "path": ""},
+                {"name": "Micro SD", "path": ""},
+                {"name": "Lecteur de carte", "path": ""},
+                {"name": "Allume Cigare + MP3", "path": ""},
+                {"name": "Cable audio", "path": ""},
+                {"name": "Tige selfie", "path": ""},
+                {"name": "Stylet", "path": ""},
+            ]
+        }, {
+            "name": "Objects Connectes", "children": [
+                {"name": "Montre connectee", "path": ""},
+                {"name": "Bracelet connecte", "path": ""},
+                {"name": "Casque VR", "path": ""}
+            ]
+        }
+    ]
+    }, {
+        "name": "Ordinateurs", "children": [
+            {
+                "name": "Ordinateur portable", "children": [
+                {"name": "PC Portable", "path": ""},
+                {"name": "PC Portable Gamer", "path": ""},
+                {"name": "MacBook", "path": ""},
+            ]
+            }, {
+                "name": "Ordinateur de bureau", "children": [
+                    {"name": "Pc de bureau", "path": ""},
+                    {"name": "Pc Gamer", "path": ""},
+                    {"name": "Imac", "path": ""}
+                ]
+            }, {
+                "name": "Accessoires", "children": [
+                    {"name": "Souris", "path": ""},
+                    {"name": "Tapis", "path": ""},
+                    {"name": "Casque Micro", "path": ""},
+                    {"name": "Clavier", "path": ""},
+                    {"name": "Sacoches", "path": ""},
+                    {"name": "Sac a dos", "path": ""},
+                    {"name": "Web Cam", "path": ""},
+                    {"name": "Manette de jeux", "path": ""},
+                    {"name": "Haut parleur", "path": ""},
+                    {"name": "Support & refroidisseur", "path": ""},
+                ]
+            }, {
+                "name": "Stockage", "children": [
+                    {"name": "Cle usb", "path": ""},
+                    {"name": "Disque dur externe", "path": ""},
+                    {"name": "Disque dur SSD", "path": ""},
+                    {"name": "Carte memoire", "path": ""},
+                    {"name": "Lecteur de carte sd", "path": ""},
+                    {"name": "CD & DVD", "path": ""},
+                ]
+            }, {
+                "name": "Logiciels", "children": [
+                    {"name": "Antivirus", "path": ""},
+                    {"name": "Systeme d'Exploitation", "path": ""},
+                    {"name": "Suite bureautique", "path": ""}
+                ]
+            }, {
+                "name": "Composants & maintenance", "children": [
+                    {"name": "Clavier", "path": ""},
+                    {"name": "Disque dur interne", "path": ""},
+                    {"name": "Chargeur", "path": ""},
+                    {"name": "Batterie", "path": ""},
+                    {"name": "Graveur", "path": ""},
+                    {"name": "Processeur", "path": ""},
+                    {"name": "RAM", "path": ""},
+                    {"name": "Bloc et cable d'alimentation", "path": ""},
+                    {"name": "Ventilateur", "path": ""},
+                    {"name": "Carte mere", "path": ""},
+                    {"name": "Carte graphique", "path": ""},
+                    {"name": "Carte reseau", "path": ""},
+                ]
+            }, {
+                "name": "Cable et Adaptateur", "children": [
+                    {"name": "Cables HDMI & SATA", "path": ""},
+                    {"name": "Cables USB", "path": ""},
+                    {"name": "Cables Firewire", "path": ""},
+                    {"name": "Cables Ecran TV/AUDIO", "path": ""},
+                    {"name": "CAbles Alimentation", "path": ""},
+                    {"name": "Adaptateurs/Conventiseseurs", "path": ""},
+                    {"name": "Connecteur RJ45 & RJ 11", "path": ""},
+                ]
+            }, {
+                "name": "Serveur & Station de travail", "children": [
+                    {"name": "Serveur tour", "path": ""},
+                    {"name": "Serveur RAck", "path": ""},
+                    {"name": "Station de travail", "path": ""}
+                ]
+            }
+        ]
+    }, {
+        "name": "Accessoires & Peripheriques", "children": [
+            {
+                "name": "Peripheriques", "children": [
+                {"name": "Souris & tapis", "path": ""},
+                {"name": "Ecouteur & micro-casque", "path": ""},
+                {"name": "Clavier", "path": ""},
+                {"name": "Haut parleur", "path": ""},
+                {"name": "Webcam", "path": ""},
+                {"name": "HUB", "path": ""},
+                {"name": "Refroidisseur", "path": ""},
+                {"name": "Nettoyage", "path": ""},
+                {"name": "Support pc portable", "path": ""},
+                {"name": "Chargeur pc portable", "path": ""},
+                {"name": "Batterie", "path": ""},
+            ]
+            }, {
+                "name": "Cable & Adaptateur", "children": [
+                    {"name": "Cables HDMI & SATA", "path": ""},
+                    {"name": "Cables USB", "path": ""},
+                    {"name": "Cables Reseau", "path": ""},
+                    {"name": "Cables Ecran TV/AuDIO", "path": ""},
+                    {"name": "Cables Firewire", "path": ""},
+                    {"name": "Cables Alimentation", "path": ""},
+                    {"name": "Adapteteurs / Convertisseurs", "path": ""}
+                ]
+            }
+        ]
+    }, {
+        "name": "Impression", "children": [
+            {
+                "name": "Monofonction", "children": [
+                {"name": "jet d'encre", "path": ""},
+                {"name": "a reservoir", "path": ""},
+                {"name": "Noir & Blanc", "path": ""},
+                {"name": "Couleur", "path": ""},
+            ]
+            }, {
+                "name": "Multifonction", "children": [
+                    {"name": "jet d'encre", "path": ""},
+                    {"name": "a reservoir", "path": ""},
+                    {"name": "Noir & Blanc", "path": ""},
+                    {"name": "Couleur", "path": ""},
+                ]
+            }, {
+                "name": "Consommables", "children": [
+                    {"name": "Cartouche", "path": ""},
+                    {"name": "Toner", "path": ""},
+                    {"name": "Bouteille d'encre", "path": ""},
+                    {"name": "Ruban", "path": ""},
+                ]
+            }
+        ]
+    },
+]
--- app/management/commands/populate_db.py
+++ app/management/commands/populate_db.py
@@ -1,19 +1,56 @@
+import random
+
 from django.core.management.base import BaseCommand
 from app.models import Category
 from app.models import Product
 from app.models import Filter
 from app.models import Details
 
+from .mock_data import mock_data
+import string
+
+
+def get_random_string():
+    letters = string.ascii_letters + "0123456789-"
+    result_str = ''.join(random.choice(letters) for i in range(25))
+    return result_str
+
 
 def _populate(self):
-    category_1 = Category(name="Computers")
-    product = Product(ref="xf7-dt65o-p2ie6-nf65", popularity=50,
-                      characteristics={'data': [{'name': 'Processor', 'value': '...'}]},
-                      category=category_1)
-    category_1.save()
-    product.save()
+    for c in mock_data:
+        category = Category(name=c['name'])
+        category.save()
+        for sc in c['children']:
+            sub_category = Category(name=sc['name'], parent=category)
+            sub_category.save()
+            for ssc in sc['children']:
+                sub_sub_category = Category(name=ssc['name'], parent=sub_category)
+                sub_sub_category.save()
+                for i in range(random.randint(15, 100)):
+                    product = Product(ref=get_random_string(), popularity=random.randint(15, 100),
+                                      characteristics={
+                                              'Processor': 'i' + str(2 * random.randint(1, 4) + 1) + '-' + str(
+                                                  random.randint(2000, 11986)) + 'H',
+                                              'Graphic card': 'NVIDIA GeForce ' + (
+                                                  'GTX' if random.randint(0, 1) % 2 == 1 else 'RTX') + ' ' + str(
+                                                  random.randint(1000, 3200)) + ' (4 Go GDDR5)',
+                                              'Screen': ['17.3"', '15.6"', '14"', '13.3"', '12"'][random.randint(0, 4)]
+                                                        + ('' if random.randint(0, 1) % 2 == 1 else 'Full') + ' HD',
 
-    self.stdout.write('is it okay ?')
+                                      },
+                                      category=sub_sub_category)
+                    product.save()
+                    for j in range(random.randint(1, 3)):
+                        details = Details(price=random.randint(1000, 7000),
+                                          owner=["myteck", "wiki", "tunisianet", "sbsInformatique"][random.randint(0, 3)],
+                                          name='MSI GF75 THIN 10SCSR',
+                                          characteristics={
+                                              'RAM': str(random.randint(2, 128)) + ' Go DDR4',
+                                              'OS': ['FreeDos', 'Ubuntu', 'Windows'][random.randint(0, 2)],
+                                              'color': ['black', 'red', 'blue', 'pink'][random.randint(0, 3)]
+                                          },
+                                          product=product)
+                        details.save()
 
 
 class Command(BaseCommand):
